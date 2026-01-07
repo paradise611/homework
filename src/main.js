e679825081fc11f3f286dc19ec5a36e200683d6c
paradise611 <1758573604@qq.com>
2026-01-07 22:35:06 +0800

    添加头图

--- src/main.js
+++ src/main.js
@@ -1,9 +1,4 @@
 import { createApp } from 'vue'
 import './style.css'
-<<<<<<< HEAD
-import App from './App.vue'
-=======
 import App from './works.vue'
->>>>>>> feature/sakura
-
 createApp(App).mount('#app')
--- src/works.vue
+++ src/works.vue
@@ -1,8 +1,10 @@
 <template>
   <Nav />
   <Sakura />
+  <headphoto />
 </template>
 <script setup>
     import Nav from './components/Nav.vue';  
     import Sakura from './components/Sakura.vue';  
+    import headphoto from './components/headphoto.vue';
 </script>
